099777111e340ad3c8cb5311c92827d16e20509f
Wladislaw Mitzel <wlami.github@tawadi.de>
2011-12-24 15:13:07 +0100

    intermediate state

--- com.wlami.mibox.client/src/com/wlami/mibox/client/metadata/MetadataRepositoryImpl.java
+++ com.wlami.mibox.client/src/com/wlami/mibox/client/metadata/MetadataRepositoryImpl.java
@@ -17,12 +17,27 @@
  */
 package com.wlami.mibox.client.metadata;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.ConcurrentSkipListSet;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.wlami.mibox.client.application.AppSettings;
+import com.wlami.mibox.client.application.AppSettingsDao;
 
 /**
  * @author Wladislaw Mitzel
@@ -35,8 +50,10 @@ public class MetadataRepositoryImpl implements MetadataRepository {
 	private static final Logger log = LoggerFactory
 			.getLogger(MetadataRepositoryImpl.class);
 
-	/** the internal file structure stored as an {@link MFolder} instance. */
-	private MFolder rootFolder;
+	/**
+	 * Reference to the {@link AppSettingsDao} bean.
+	 */
+	AppSettingsDao appSettingsDao;
 
 	/**
 	 * reference to a {@link MetadataWorker} instance which handles the file
@@ -49,6 +66,14 @@ public class MetadataRepositoryImpl implements MetadataRepository {
 	 * processed by the {@link MetadataWorker}.
 	 */
 	private ConcurrentSkipListSet<ObservedFilesystemEvent> incomingEvents = new ConcurrentSkipListSet<ObservedFilesystemEvent>();
+
+	/**
+	 * default constructor.
+	 */
+	@Inject
+	public MetadataRepositoryImpl(AppSettingsDao appSettingsDao) {
+		this.appSettingsDao = appSettingsDao;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -106,12 +131,86 @@ public class MetadataRepositoryImpl implements MetadataRepository {
 	 */
 	class MetadataWorker extends Thread {
 
+		/**
+		 * 
+		 */
+		private static final String METADATA_DEFAULT_FILENAME = ".mibox";
+
 		private final Logger log = LoggerFactory
 				.getLogger(MetadataWorker.class);
 
 		private boolean active = true;
 
 		private static final long DEFAULT_SLEEP_TIME = 250L;
+
+		/** the internal file structure stored as an {@link MFolder} instance. */
+		private MFolder rootFolder;
+
+		/**
+		 * 
+		 */
+		public MetadataWorker() {
+			AppSettings appSetting;
+			try {
+				appSetting = appSettingsDao.load();
+				ObjectMapper objectMapper = new ObjectMapper();
+				File metadataFile = new File(METADATA_DEFAULT_FILENAME) {
+				};
+				if (metadataFile.exists()) {
+					// read the metadata from disk
+					rootFolder = objectMapper.readValue(new BufferedReader(
+							new FileReader(metadataFile)), MFolder.class);
+				} else {
+					// create a new metadata file
+					rootFolder = new MFolder(null);
+					rootFolder.setName("/");
+					objectMapper.writeValue(metadataFile, rootFolder);
+				}
+				synchronizeMetadata();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		protected void synchronizeMetadata() throws IOException {
+			AppSettings appSettings = appSettingsDao.load();
+			String watchDir = appSettings.getWatchDirectory();
+			Files.walkFileTree(Paths.get(watchDir),
+					new SimpleFileVisitor<Path>() {
+
+						/*
+						 * (non-Javadoc)
+						 * 
+						 * @see
+						 * java.nio.file.SimpleFileVisitor#preVisitDirectory
+						 * (java.lang.Object,
+						 * java.nio.file.attribute.BasicFileAttributes)
+						 */
+						@Override
+						public FileVisitResult preVisitDirectory(Path dir,
+								BasicFileAttributes attrs) throws IOException {
+							// TODO Auto-generated method stub
+							return super.preVisitDirectory(dir, attrs);
+						}
+
+						/*
+						 * (non-Javadoc)
+						 * 
+						 * @see
+						 * java.nio.file.SimpleFileVisitor#visitFile(java.lang
+						 * .Object, java.nio.file.attribute.BasicFileAttributes)
+						 */
+						@Override
+						public FileVisitResult visitFile(Path file,
+								BasicFileAttributes attrs) throws IOException {
+							// TODO Auto-generated method stub
+							return super.visitFile(file, attrs);
+						}
+
+					});
+		}
 
 		/*
 		 * (non-Javadoc)
